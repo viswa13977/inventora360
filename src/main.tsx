674ce0f5739b72239285d4b6e061ff87f471d973
swapnil.devkar <swapnil.devkar@fyntune.com>
2025-08-17 15:20:09 +0530

    deploy ci cd

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 // ✅ Temporarily disable StrictMode
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
--- src/pages/tenant/Dashboard.tsx
+++ src/pages/tenant/Dashboard.tsx
@@ -1,5 +1,5 @@
 // pages/tenant/Dashboard.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useTenantAuth } from '../../auth/TenantAuth';
 
